a064981aa64bfb217e282d1434f8c0405c1b137f
Sarath Kumar M <sarath@blaze.ws>
2023-02-23 16:22:44 +0530

    claim flow and add email feature

---
+++ frontend/src/components/Claim_Flow_Account.jsx
@@ -0,0 +1,11 @@
+import { React, useState, useEffect, useMyCustomStuff } from 'react';
+
+const ClaimFlowAccount = () => {
+  return (
+    <div className='scrolldiv1'>
+      <p>Claim flow account page</p>
+    </div>
+  );
+}
+
+export default ClaimFlowAccount;

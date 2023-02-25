20968224baf8b163cf9e79fa62ea061ca398f274
Yuvarajan <yuvarajan@blaze.ws>
2023-02-25 18:56:54 +0530

    Created design for 404 page

--- frontend/src/components/PageNotFound.jsx
+++ frontend/src/components/PageNotFound.jsx
@@ -1,9 +1,14 @@
 import { React } from 'react';
-
+import { NavLink } from 'react-router-dom';
 
 const PageNotFound = () => {
   return (
-    <div>Page Not Found - 404</div>
+    <div className='pnf'>
+        <img src={require('../assets/images/page-not-found.png')} loading="lazy" />
+        <h6>Oops!</h6>
+        <p>The page you were looking for doesn't exist</p>
+        <NavLink to="/dashboard" className='btn btn-primary'>Go to Dashboard</NavLink>
+    </div>
   );
 }
 

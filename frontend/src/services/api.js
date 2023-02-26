199e24f9149421921067a3b5c084e41c86da836f
Vijaykoushik Sekar <vijaykoushik@blaze.ws>
2023-02-26 10:22:31 +0530

    fix: Fix api endpoint
    Added correct endpoint for api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5007/',
+  baseURL: 'https://api.certifi.ly/',
   headers: {
     'Content-Type': 'application/json',
   },

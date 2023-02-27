74afa1b9d6b57a171bb1c11816c3a3325bbbee2d
Vijaykoushik Sekar <vijaykoushik@blaze.ws>
2023-02-27 15:57:32 +0530

    fix: Fix build error with package pdf-img-convert

--- backend/src/app/scripts/pdf2image.ts
+++ backend/src/app/scripts/pdf2image.ts
@@ -2,7 +2,7 @@ import { existsSync } from 'fs';
 import { writeFile } from 'fs/promises';
 import { basename, dirname } from 'path';
 // import { pdf } from 'pdf-to-img';
-import pdf2img from 'pdf-img-convert';
+// import pdf2img from 'pdf-img-convert';
 import { exit } from 'process';
 
 async function main(args: string[]) {
@@ -14,8 +14,8 @@ async function main(args: string[]) {
     }
     // const pages = await pdf(path);
     // await writeFile(basename(path, '.pdf') + 'hello.png', pages);
-    const buff = await pdf2img.convert(path);
-    await writeFile(basename(path, '.pdf') + 'hello.png', buff);
+    // const buff = await pdf2img.convert(path);
+    // await writeFile(basename(path, '.pdf') + 'hello.png', buff);
 }
 
 const args = process.argv.slice(2);

b138aa3ff285c0afc530331f852de59d6ef8c874
Vijaykoushik Sekar <vijaykoushik@blaze.ws>
2023-02-28 13:38:05 +0530

    Update text in create-token page

--- frontend/src/components/Create_Api_Token.jsx
+++ frontend/src/components/Create_Api_Token.jsx
@@ -4,6 +4,9 @@ import * as eva from 'eva-icons';
 
 const CreateApiToken = () => {
 
+  const [copySuccess,setCopySucess] = useState(false);
+  let timeout;
+
   const user_email = localStorage.getItem('user_email');
   const accessToken = localStorage.getItem('accessToken');
   let userprofile = JSON.parse(localStorage.getItem('userprofile'));
@@ -35,6 +38,11 @@ const CreateApiToken = () => {
     const input = document.querySelector('input');
     input.select();
     document.execCommand('copy');
+    setCopySucess(true);
+    clearTimeout(timeout);
+    timeout = setTimeout(()=>{
+      setCopySucess(false)
+    },4000)
   };
 
   useEffect(() => {eva.replace()})
@@ -56,14 +64,14 @@ const CreateApiToken = () => {
         <div className='col-md-12 text-start'>
           <div className='row mb-3 align-items-center addemailaccount'>
             <div className='col-md-12 mb-3'>
-              <div className='backgroundblur'>
+              {/* <div className='backgroundblur'>
                 <h6 className="fw-bolder text-black text-uppercase mb-3">Create Token</h6>
                 <form onSubmit={handleSubmit}>
                   <div className="row">                   
                     <div className='col-md-6'>
                     <div className="mb-3 btngrouprht resbtn">
-                        <button type="submit" className='btn btn-primary btn-icon'> Generate Public Token</button>
-                        <button type="submit" className='btn btn-primary btn-icon'> Generate Private Token</button>
+                        <button type="submit" className='btn btn-primary btn-icon'> Generate Public Key</button>
+                        <button type="submit" className='btn btn-primary btn-icon'> Generate Secret Key</button>
                       </div>
                     <div className="input-group mb-3 inputbtngroup">
                           <input type={'text'} name="email" value={inputValue} onChange={e => setInputValue(e.target.value)} className="form-control" placeholder='' />
@@ -72,22 +80,45 @@ const CreateApiToken = () => {
                       </div>                          
                   </div>
                 </form>
+              </div> */}
+
+            <div className="mb-3 mt-3 btngrouprht resbtn float-end">
+                  <button
+                      type="submit"
+                      className="btn btn-primary btn-icon"
+                  >
+                      {' '}
+                      Generate Public Key
+                  </button>
+                  <button
+                      type="submit"
+                      className="btn btn-primary btn-icon"
+                  >
+                      {' '}
+                      Generate Secret Key
+                  </button>
               </div>
             </div>
-
             <div className='col-md-12 mb-3'>
               <div className='backgroundblur'>
-                <h6 className="fw-bolder text-black text-uppercase">Active Tokens</h6>
-
+                <h6 className="fw-bolder text-black text-uppercase">API keys</h6>
+                {copySuccess && (
+                    <div
+                        class="alert alert-success text-center col-sm-6 mx-auto py-3 fade show"
+                        role="alert"
+                    >
+                        Secret key copied to clipboard!
+                    </div>
+                )}
                 <div className='tableblur mt-4'>
                   <div className='table-responsive'>
                     <table className="table align-middle mb-0 custable table-hover">
                       <thead className="">
                         <tr>
                           <th></th>
-                          <th width="700">Token</th>
+                          <th width="700">Secret key</th>
                           <th>Created on</th>
-                          <th>Verification</th>
+                          <th>Status</th>
                           {/* <th className='text-center d-none'>Action</th> */}
                         </tr>
                       </thead>
@@ -101,13 +132,34 @@ const CreateApiToken = () => {
                           </td>
                           <td className='accessToken'>
                             {/* {"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTIzNDU2Nzg5LCJuzI1NiIsInR5cCI6IkpXLCJuzI1NiIsInR5cCI6I"} */}
-                            <span>{accessToken}</span>
+                            {/* <span>{accessToken}</span> */}
+                            <div className="input-group mb-3 inputbtngroup">
+                              <input 
+                                type={'text'}
+                                name="apiKey" 
+                                value={inputValue} 
+                                onChange={e => setInputValue(e.target.value)}
+                                className="form-control"
+                                placeholder='' 
+                                disabled={true}
+                              />
+                              <button 
+                                onClick={handleCopy} 
+                                type="submit" 
+                                className='btn btn-primary btn-icon'
+                              >
+                                <i data-eva="copy-outline"></i> Copy Secret
+                              </button>
+                            </div>
+                            <form>
+
+                            </form>
                           </td>
                           <td>
                             {"February 25, 2023"}
                           </td>
                           <td>
-                            <span className='badge badge-success'>VERIFIED</span>
+                            <span className='badge badge-success'>ACTIVE</span>
                           </td>
 
                           {/* <td className='text-center'>

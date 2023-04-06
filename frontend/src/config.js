1ea680e63a22890f9bbb60fc7b68e2b453ce3387
shunmugam <sundaram.blaze@gmail.com>
2023-04-06 16:42:06 +0530

    singup updation

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -8,8 +8,8 @@ config()
 .put("accessNode.api", process.env.REACT_APP_ACCESS_NODE) // Configure FCL's Access Node  
 .put("discovery.wallet", process.env.REACT_APP_WALLET_DISCOVERY) // Configure FCL's Wallet Discovery mechanism  
 .put("flow.network", "testnet")
-.put("app.detail.icon", "https://devnft.disrupt.art/logo512.png")
-.put("app.detail.title", "DisruptArt") 
+.put("app.detail.icon", "https://alpha.certifi.ly/logo512.png")
+.put("app.detail.title", "Certifily") 
 .put("discovery.wallet.method.default","POP/RPC")
 
 

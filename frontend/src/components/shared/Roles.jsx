0ade55ff112de691223843cf638fa79ae1386c88
shunmugam <sundaram.blaze@gmail.com>
2023-04-12 18:24:07 +0530

    update new roles

--- frontend/src/components/shared/Roles.jsx
+++ frontend/src/components/shared/Roles.jsx
@@ -1,3 +1,2 @@
-export const mainRoles = ["preparer","verifier","issuer"]
+export const mainRoles = ["preparer","verifier","issuer","organisation adminstrator"]
 export const userRoles = "user" 
-  
